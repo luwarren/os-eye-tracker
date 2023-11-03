12b7777c1598690f0034e6234d998e39b16c2a5e
luwarren <warrenlu2002@gmail.com>
2023-11-03 23:40:15 +1100

    Working isolated each eye

--- track.py
+++ track.py
@@ -1,96 +1,84 @@
 import cv2
 import numpy as np
 
+class FaceEyeDetector:
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        self.eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+        self.detector_params = cv2.SimpleBlobDetector_Params()
+        self.detector_params.filterByArea = True
+        self.detector_params.maxArea = 1500
+        self.detector = cv2.SimpleBlobDetector_create(self.detector_params)
 
-# init part
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-detector_params = cv2.SimpleBlobDetector_Params()
-detector_params.filterByArea = True
-detector_params.maxArea = 1500
-detector = cv2.SimpleBlobDetector_create(detector_params)
-
-
-def detect_faces(img, cascade):
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    coords = cascade.detectMultiScale(gray_frame, 1.3, 5)
-    if len(coords) > 1:
-        biggest = (0, 0, 0, 0)
-        for i in coords:
-            if i[3] > biggest[3]:
-                biggest = i
-        biggest = np.array([i], np.int32)
-    elif len(coords) == 1:
-        biggest = coords
-    else:
+    def detect_faces(self, img):
+        gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        coords = self.face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+        if len(coords) > 0:
+            biggest = max(coords, key=lambda x: x[3])
+            x, y, w, h = biggest
+            frame = img[y:y + h, x:x + w]
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
+            return frame
         return None
-    for (x, y, w, h) in biggest:
-        frame = img[y:y + h, x:x + w]
-    return frame
 
+    def detect_eyes(self, img):
+        gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        eyes = self.eye_cascade.detectMultiScale(gray_frame, 1.3, 5)
+        width = img.shape[1]
+        height = img.shape[0]
+        left_eye = None
+        right_eye = None
+        for x, y, w, h in eyes:
+            if y > height / 1.5:
+                continue
+            eyecenter = x + w / 2
+            if eyecenter < width * 0.5:
+                left_eye = img[y:y + h, x:x + w]
+            else:
+                right_eye = img[y:y + h, x:x + w]
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
+        return left_eye, right_eye
 
-def detect_eyes(img, cascade):
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    eyes = cascade.detectMultiScale(gray_frame, 1.3, 5)  # detect eyes
-    width = np.size(img, 1)  # get face frame width
-    height = np.size(img, 0)  # get face frame height
-    left_eye = None
-    right_eye = None
-    for (x, y, w, h) in eyes:
-        if y > height / 2:
-            pass
-        eyecenter = x + w / 2  # get the eye center
-        if eyecenter < width * 0.5:
-            left_eye = img[y:y + h, x:x + w]
-        else:
-            right_eye = img[y:y + h, x:x + w]
-    return left_eye, right_eye
-
-
-def cut_eyebrows(img):
-    height, width = img.shape[:2]
-    eyebrow_h = int(height / 4)
-    img = img[eyebrow_h:height, 0:width]  # cut eyebrows out (15 px)
-
-    return img
-
-
-def blob_process(img, threshold, detector):
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, img = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY)
-    img = cv2.erode(img, None, iterations=2)
-    img = cv2.dilate(img, None, iterations=4)
-    img = cv2.medianBlur(img, 5)
-    keypoints = detector.detect(img)
-    print(keypoints)
-    return keypoints
-
-
-def nothing(x):
-    pass
-
+    @staticmethod   
+    def cut_eyebrows(img):
+        height, width = img.shape[:2]
+        eyebrow_h = int(height / 4)
+        return img[eyebrow_h:height, 0:width]
 
 def main():
     cap = cv2.VideoCapture(0)
     cv2.namedWindow('image')
-    cv2.createTrackbar('threshold', 'image', 0, 255, nothing)
+    detector = FaceEyeDetector()
+
     while True:
         _, frame = cap.read()
-        face_frame = detect_faces(frame, face_cascade)
+        face_frame = detector.detect_faces(frame)
+
         if face_frame is not None:
-            eyes = detect_eyes(face_frame, eye_cascade)
-            for eye in eyes:
-                if eye is not None:
-                    threshold = r = cv2.getTrackbarPos('threshold', 'image')
-                    eye = cut_eyebrows(eye)
-                    keypoints = blob_process(eye, threshold, detector)
-                    eye = cv2.drawKeypoints(eye, keypoints, eye, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        cv2.imshow('image', frame)
+            eyes = detector.detect_eyes(face_frame)
+            left_eye, right_eye = eyes
+
+            if left_eye is not None and right_eye is not None:
+
+                # Ensure the left and right eyes have the same height
+                min_height = min(left_eye.shape[0], right_eye.shape[0])
+                left_eye = left_eye[:min_height, :]
+                right_eye = right_eye[:min_height, :]
+
+                # Resize the eyes to have the same width
+                width = max(left_eye.shape[1], right_eye.shape[1])
+                left_eye = cv2.resize(left_eye, (width, min_height))
+                right_eye = cv2.resize(right_eye, (width, min_height))
+
+                # Display the two eyes side by side
+                eyes_combined = np.hstack((left_eye, right_eye))
+                cv2.imshow("image", eyes_combined)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
     cap.release()
     cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     main()
